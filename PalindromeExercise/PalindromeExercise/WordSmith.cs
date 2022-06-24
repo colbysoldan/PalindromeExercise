7e49ba4cedf17398df1f3af00271ececaac32a0a
Colby Soldan <colbylsoldan@gmail.com>
2022-06-24 10:59:56 -0500

    made test

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    { 
+
+        public bool IsAPalindrome(string word)
+        {
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            var reverse = new string(arr);
+
+            return word == reverse;
+
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("car", false)]
+        [InlineData("asdfgfdsa", true)]
 
+        public void PalindromeTest(string word, bool expected)
+        {
+            //Arrange
+            var test = new WordSmith();
+
+            //Act
+            var actual = test.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
